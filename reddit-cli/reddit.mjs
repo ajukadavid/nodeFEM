2e5ab007d68199393ff14491d7577e50af752824
ithiel883 <ajuka.zephiniah@gmail.com>
2023-01-04 14:23:08 +0100

    succesful integration of reddit api

--- reddit-cli/reddit.mjs
+++ reddit-cli/reddit.mjs
@@ -8,5 +8,15 @@ const { argv } = yargs(process.argv)
 const res = await fetch('https://reddit.com/.json')
 const data = await res.json()
 
+const children = data.data.children
+const randomPost = children[Math.floor(Math.random() * children.length)]
+const link = `https://reddit.com${randomPost.data.permalink}`
 
-console.log(data)
+if(argv.print){
+    console.log(`
+        title: ${randomPost.data?.title}   
+        link: ${link}
+    `)
+} else {
+    open(link)
+}
