d09a21f0e27d922c5bb1022f939673efcef0a184
ithiel883 <ajuka.zephiniah@gmail.com>
2023-01-04 10:33:21 +0100

    added obj.entries and replace to add dynamic HTML

--- index.mjs
+++ index.mjs
@@ -1,3 +1,14 @@
-import fs from 'fs/promises'
+import { readFile  } from 'fs/promises'
 
-console.log(fs)
+let template = await readFile(new URL('template.html', import.meta.url), 'utf-8')
+
+const data = {
+    title: 'Learn Node.js',
+    body: 'This is the final HTML'
+}
+
+for (const [k, v] of Object.entries(data)){
+    template = template.replace(`{${k}}`, v)
+}
+
+console.log(template)
