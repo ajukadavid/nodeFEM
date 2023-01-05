e3a0c4040af6a3e46c2d528c82b3b1ab0acbda64
ithiel883 <ajuka.zephiniah@gmail.com>
2023-01-05 11:47:53 +0100

    setup of express server

--- server/server.mjs
+++ server/server.mjs
@@ -7,3 +7,25 @@ const app = express()
 app.use(bp.urlencoded({extended: true }))
 app.use(bp.json())
 app.use(morgan())
+
+const db = []
+
+app.post('/todo', (req, res) => {
+    const newTodo = {
+         id: Date.now(),
+         text: req.body.text
+    }
+
+    db.push(newTodo)
+
+    res.json(newTodo)
+       
+})
+
+app.get('todo', (req, res) => {
+    res.json(db)
+})
+
+app.listen(8000, () => {
+    console.log('Server on http://localhost:8000')
+})
