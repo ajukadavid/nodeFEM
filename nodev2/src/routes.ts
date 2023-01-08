0ae20bc3ea8d614a7e9a94930616c098e03fd98a
ithiel883 <ajuka.zephiniah@gmail.com>
2023-01-08 23:43:44 +0100

    finished setting up routes

---
+++ nodev2/src/routes.ts
@@ -0,0 +1,60 @@
+import { Router } from 'express'
+
+const router = Router()
+
+//PRODUCTS
+
+router.get('/products', () => {
+
+})
+router.get('/product/:id', () => {
+    
+})
+router.put('/product/:id', () => {
+    
+})
+router.post('/product', () => {
+    
+})
+
+router.delete('/product/:id', () => {
+    
+})
+
+//UPDATES
+
+router.get('/update', () => {
+
+})
+router.get('/update/:id', () => {
+    
+})
+router.put('/update/:id', () => {
+    
+})
+router.post('/update', () => {
+    
+})
+
+router.delete('/update/:id', () => {
+    
+})
+
+//UPDATES
+
+router.get('/updatepoint', () => {
+
+})
+router.get('/updatepoint/:id', () => {
+    
+})
+router.put('/updatepoint/:id', () => {
+    
+})
+router.post('/updatepoint', () => {
+    
+})
+
+router.delete('/updatepoint/:id', () => {
+    
+})
