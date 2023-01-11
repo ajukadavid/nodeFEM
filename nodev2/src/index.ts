6d466e5ab429e5a9160aac587dbac92db9c1faa3
ithiel883 <ajuka.zephiniah@gmail.com>
2023-01-11 21:44:04 +0100

    moved v2 folder out

--- nodev2/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-import config from './config'
-
-import app from './server'
-
-app.listen(config.port, () => {
-    console.log(`hello on http://localhost:${config.port}`)
-})
